c4daec6b0d81792c09636845f9ec6ffdd36430d4
kusuma bc <94009388+9632709791@users.noreply.github.com>
2022-05-20 22:52:03 +0530

    Add files via upload

--- clickaholic-master/js/custom.js
+++ clickaholic-master/js/custom.js
@@ -13,3 +13,25 @@ $('#return-to-top').click(function () {
         scrollTop: 0 // Scroll to top of body
     }, 500);
 });
+// for FAQ //
+
+const accordionBtns = document.querySelectorAll(".item-header");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
+
